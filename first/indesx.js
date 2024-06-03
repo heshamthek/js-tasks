619873f3e5c27fd94e580b54c43eb97da7df44f6
heshamthek <hnajem133@gmail.com>
2024-06-03 12:12:39 +0300

    done

---
+++ first/indesx.js
@@ -0,0 +1,123 @@
+/*
+let q= "hesham";
+let w = true;
+let r = 20;
+let hesham= ["ok" , "gg"];
+let cars = {carname:"bmw" , type:"suv"};
+
+var iiii= "cool";
+var s= false;
+var d1 = 10;
+var k1 = [ "coolapsdkopmas"];
+var g1= { firstname:"pasniud" , age:"80"};
+
+const a= "najem";
+const s1= false;
+const d = 16;
+const k = [ "ppppp" , "pomodas"];
+const g= { firstname:"hesham" , age:"99"};
+
+var z= "cool";
+var x= false;
+var c = 10;
+var v = [ "coolapsdkopmas"];
+var b= {webname:"google" , type:"se"};
+
+let ee= "hesham";
+let qq = true;
+let rr = 20;
+let tt= ["ok" , "gg"];
+let yy = {carname:"volvo" , type:"sedan"};
+
+const mm= "najem";
+const jj= false;
+const uu = 16;
+const oo = [ "asdghh" , "pomodas"];
+const pp= { firstname:"najem" , age:"81"};
+
+const sss= "google"
+const ddd= false
+const fff = 1651
+const ggg = [ "asfqwe" , "gdfxcv"]
+const ttt= { firstname:"sami" , age:"156"}
+
+let zzz= "hesham";
+let xxx = true;
+let rcc = 898;
+let vvvv= ["warerrrr" , "3oooo"];
+let bbbb = {firstname:"sami" , age:"156"};
+*/
+
+function varScoping() {
+    var y1 = 1;
+
+    if (true) {
+      var y1 = 4;
+      console.log(y1); 
+    } 
+    console.log(y1);
+  }
+ varScoping()
+  
+  function letScoping() {
+    let x1 = 5;
+  
+    if (true) {
+      let x1 = 3;   
+      console.log(x1); 
+    }
+    console.log(x1); 
+  }
+  letScoping()
+ //task7 1 Arithmetic Operations
+  let a2= 10
+  let d2=5
+  console.log( a2+d2)
+  console.log(a2-d2)
+  console.log(a2*d2)
+  console.log(a2/d2)
+  console.log(a2%d2)
+
+//⦁	Comparison Operations
+  console.log(a2>d2)
+  console.log(a2<d2)
+  console.log(a2==d2)
+  console.log(a2!=d2)
+  console.log(a2>=d2)
+  console.log(a2<=d2)
+//⦁	Logical Operations
+  console.log(a2+= 5)
+  console.log(a2 -= 5)
+  console.log(a2 *= 2)
+ 
+
+
+//⦁	Assignment Operations
+let a3 =true
+let d3 = false
+  console.log(a2&&d2)
+  console.log(a2||d2)
+  console.log(!a3)
+  console.log(!d3)
+
+
+//task8
+
+  //window.alert()
+  //document.write("good")
+  // done
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+ 
