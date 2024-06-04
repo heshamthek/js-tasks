a4cc63cfd971cefa1900f385f74cbb42cbd9f7a5
heshamthek <hnajem133@gmail.com>
2024-06-04 22:32:04 +0300

    done

---
+++ 3/index.js
@@ -0,0 +1,72 @@
+//task1 
+// function sayhello(){ 
+//     let namee="bob"
+//     console.log("hello", `${namee}` )
+// }
+// sayhello()
+
+// task2
+
+//  function square(x){
+//   let g =  Math.pow(x, 2)
+//    return console.log( g); 
+//  }
+//  square(5)
+ 
+// task3
+//  multiply = (x,y) => x * y
+//  console.log(multiply(4,5))
+
+
+// task4
+//  let car={ make:"toyota",model:"camry",year:"2020", 
+//  getcarinfo: function(){
+//     return`${this.make} ${this.model} ${this.year}`
+//  },
+
+//  };
+// console.log(car.getcarinfo())
+
+
+// task5
+
+// var colors= [ "green","white","yello"]
+// for(let i in colors){ 
+//     console.log(colors[i])
+// }
+
+// tsk6
+// function st(word ){
+    
+//     return  word.length;
+// }
+// console.log(st("hello"))
+
+
+// task7
+
+// function up(connn){
+  
+//     return connn.toUpperCase();
+
+// }
+// console.log(up("hello"))
+
+
+// task8
+// function st( word ,ss){
+//     let g= word.split(ss)
+//     return g ;
+// }
+// console.log( st("hello,world", ','))
+
+
+
+
+
+
+
+
+
+
+
